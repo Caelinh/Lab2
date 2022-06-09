a2cd8cb8fae546dee23ec7ea0aee48115d77baa1
Caelinh <Caelinhargrave@gmail.com>
2022-06-09 17:57:36 -0500

    Added a function

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ console.log(userName);
 document.write("Hello " + userName + " A " + "few " + "things " + "about " + "me...");
 
 
+function Questions(){
 
-if (confirm("Hi! Was I a ballet dancer?")){
-    alert("You are right, I was!");
-} else { alert("I actually was!");
+let question1 = prompt("Hi! Was I a ballet dancer?");
+if (question1 == "yes"){
+    alert("Your right!")
+}else(question1 == "no"){
+    alert("You're wrong");
 }
 
-if (confirm("Do I have a daughter?")){
+//     if (confirm("Hi! Was I a ballet dancer?")){
+//     alert("You are right, I was!");
+// } else { alert("I actually was!");
+}
+
+
+if (prompt("Do I have a daughter?")){
     alert("Yes, she is the light of my life");
 } else { alert("I am glad you are wrong!");
 }
@@ -32,7 +41,7 @@ if (confirm("Do I love coding?")){
 } else { document.write("Wrong answer, I love coding!");
     
 }
-
+    
 
     let dog = [''];
 prompt('how many dogs do i have between 1-4?');
@@ -41,8 +50,8 @@ if ('2') {
 } else { alert("no, try again!");
 }
 
-
-
+}
+Questions();
 
 
 let questions = [
@@ -51,20 +60,32 @@ let questions = [
     'Am I a Sagittarius?'
 ];
 
+let answers = [
+    "SEATTLE",
+    "2",
+    "YES"
+]
+
 let responses = [];
 for (let i =0; i < questions.length; i++) {
-     responses[i] = prompt(questions[i]);
+     responses[i] = prompt(questions[i]).toUpperCase();
+     checkAnswers(i);
     
 }
 console.log("responses", responses);
 
-let manyDogs = prompt('How many dogs do you have?');
+function checkAnswers(qNumber){
+    if (responses[qNumber] === answers[qNumber] ){
+        alert("you got it right");
+    } else alert("You're wrong")
+    }
+// let manyDogs = prompt('How many dogs do you have?');
 
-let nnumber = manyDogs
- for (let i = 0; i < manyDogs; i++) {
-      alert(number);
-   console.log('I' + 'have' + [i+1] + 'dogs');
- } 
+// let nnumber = manyDogs
+//  for (let i = 0; i < manyDogs; i++) {
+//       alert(number);
+//    console.log('I' + 'have' + [i+1] + 'dogs');
+//  } 
 
 
 
