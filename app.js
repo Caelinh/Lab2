456a2e36955fdd5cb554904649873825b6435039
Erin Ranta <erinran99@gmail.com>
2022-06-09 11:31:37 -0400

    test

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 
 
-let userName = prompt("What is your name?");
+let userName = prompt("What is your name? Get to know me a bit...");
 console.log(userName);
 document.write("Hello " + userName + " A " + "few " + "things " + "about " + "me...");
 
 
 
-if (confirm("Was I a ballet dancer?")){
+if (confirm("Hi! Was I a ballet dancer?")){
     alert("You are right, I was!");
 } else { alert("I actually was!");
 }
@@ -32,4 +32,43 @@ if (confirm("Do I love coding?")){
 } else { document.write("Wrong answer, I love coding!");
     
 }
+
+
+    let dog = [''];
+prompt('how many dogs do i have between 1-4?');
+if ('2') {
+    alert("yes!");
+} else { alert("no, try again!");
+}
+
+
+
+
+
+let questions = [
+    'Where did I grow up?',
+    'How many dogs do I have?',
+    'Am I a Sagittarius?'
+];
+
+let responses = [];
+for (let i =0; i < questions.length; i++) {
+     responses[i] = prompt(questions[i]);
+    
+}
+console.log("responses", responses);
+
+let manyDogs = prompt('How many dogs do you have?');
+
+let nnumber = manyDogs
+ for (let i = 0; i < manyDogs; i++) {
+      alert(number);
+   console.log('I' + 'have' + [i+1] + 'dogs');
+ } 
+
+
+
+
+
+
 
